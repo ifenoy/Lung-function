86ca2b919919a01702f5b867ed5aa2dd14ddde06
EzequielCanay <ezecanay@gmail.com>
2023-07-03 16:07:11 -0300

    .exe
    .exe

---
+++ processing_software.cmd
@@ -0,0 +1,6 @@
+
+@echo off
+rem This script was created by Nuitka to execute 'processing_software.exe' with Python DLL being found.
+set PATH=c:\users\ezeca\appdata\local\programs\python\PYTHON~2;%PATH%
+set PYTHONHOME=C:\Users\ezeca\AppData\Local\Programs\Python\Python310
+"%~dp0.\processing_software.exe" %*
